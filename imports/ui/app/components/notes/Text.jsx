2ec5abaf37e9c57f61c70fc019146342cf0f057e
Dorian Voegeli <dvoegeli@gmu.edu>
2017-04-09 10:57:58 -0400

    Text box has focus when created

--- imports/ui/app/components/notes/Text.jsx
+++ imports/ui/app/components/notes/Text.jsx
@@ -2,13 +2,16 @@ import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import classNames from 'classnames';
 import _ from 'lodash';
-import d3 from 'd3';
 import AppState from '/imports/api/appState';
 import Colors from '/imports/api/colors';
 import Sizes from '/imports/api/sizes';
 
 // Text component - text for the notes layer
 class Text extends Component {
+  constructor(props){
+    super(props);
+    this.defaultText = 'Enter text';
+  }
   handleEdit(event) {
     event.stopPropagation();
     const text = $(this.textInput).text();
@@ -20,10 +23,23 @@ class Text extends Component {
     if(event.type === 'onBlur' || event.charCode == 13){
       event.preventDefault();
       event.target.blur();  
+      const text = $(this.textInput).text();
       const { _id, data, size, color, type } = this.props;
-      data.text = $(this.textInput).text();
       const note = { _id, type, data, color, size };
-      Meteor.call(`${note.type}.update`, note, /*handleStickyNote*/);
+      if(text){
+        data.text = text;
+        Meteor.call(`${note.type}.update`, note, /*handleStickyNote*/);
+      } else {
+        Meteor.call('notes.remove', note._id);
+      }
+    }
+  }
+  componentDidMount(){
+    const text = $(this.textInput).text();
+    const hasDefaultText = _.isEqual(text, this.defaultText);
+    if (hasDefaultText) {
+      $(this.textInput).focus();
+      $(this.textInput).select();
     }
   }
   render() {
@@ -31,7 +47,7 @@ class Text extends Component {
     const color = Colors.getHex(this.props.color);
     const size = Sizes.getText(this.props.size);
     const { coords } = this.props.data;
-    const text = this.props.data.text || "Enter text";
+    const text = this.props.data.text || this.defaultText;
     const { x, y } = this.props.data.coords[1];
     const width = Math.min(text.length+1, 50);
     const textStyle = {
