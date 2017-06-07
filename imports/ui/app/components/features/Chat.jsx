7b2e7f9429693b6c01d11e3f49a73ab4db1c0f41
Dorian Voegeli <dvoegeli@gmu.edu>
2017-06-06 21:46:39 -0400

    Add group chat and temp chat reset button

--- imports/ui/app/components/features/Chat.jsx
+++ imports/ui/app/components/features/Chat.jsx
@@ -1,13 +1,71 @@
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import classNames from 'classnames';
-import DummyText from '/imports/api/dummyText.js';
-import AppState from '/imports/api/appState.js';
+import DummyText from '/imports/api/dummyText';
+import { Chats } from '/imports/api/chats';
+import AppState from '/imports/api/appState';
+import _ from 'lodash';
 
 // Chat component - menu for chat features
 class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.lastMessage;
+    this.state = {
+      message: '',
+    };
+  }
+  componentDidMount() {
+    if (this.lastMessage) {
+      this.lastMessage.scrollIntoView();
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.lastMessage) {
+      this.lastMessage.scrollIntoView();
+    }
+  }
   closeMenu(){
     AppState.set('features_show', 'none');
+  }
+  handleInput(event){
+    const message = event.target.value;
+    this.setState({message});
+  }
+  handleSubmit(event){
+    const enterKey = _.isEqual(event.key.toUpperCase(), 'ENTER');
+    if(enterKey){
+      event.preventDefault();
+      Meteor.call('chats.insert',{
+        username: this.props.username,
+        text: this.state.message,
+      })
+      this.setState({message: ''});
+    }
+  }
+  generateMessages(){
+    const chats = this.props.chats;
+    if(_.isEmpty(chats)) return;
+
+    const soleMessage = _.isEqual(chats.length, 1);
+    const rest = soleMessage ? chats : _.dropRight(chats);
+    const last = _.last(chats);
+    
+    const messages = rest.map((message) => (
+      <li key={message._id}>
+        <p className="w3-slim w3-text-dark-grey"><b>{message.username}: </b>{message.text}</p>
+      </li>
+    ));
+
+    if(!soleMessage){
+      messages.push((
+        <li key={last._id} ref={element => this.lastMessage = element}>
+          <p className="w3-slim w3-text-dark-grey"><b>{last.username}: </b>{last.text}</p>
+        </li>
+      ));
+    } 
+    return messages;
   }
   render() {
     const chat = classNames(
@@ -20,26 +78,24 @@ class Chat extends Component {
           <a className="w3-teal w3-left-align" onClick={()=> this.closeMenu()}>
             <i className="fa-chevron-left fa fa-lg fa-fw w3-margin-right"/>
             Group Chat
+            <i className="fa-trash fa fa-lg fa-fw w3-margin-left" onClick={(event)=>{
+              event.stopPropagation();
+              Meteor.call('chats.reset');
+            }}/>
           </a>
         </header>
         <main className="panel__content w3-container">
           <ul className="w3-ul">
-            <li>
-              <p className="w3-slim w3-text-dark-grey"><b>dorian: </b>Hi, everyone!</p>
-            </li>
-            <li>
-              <p className="w3-slim w3-text-dark-grey"><b>dorian: </b>Anyone here like Lorem Ipsum text?</p>
-            </li>
-            <li>
-              <p className="w3-slim w3-text-dark-grey"><b>student: </b>Oh yeah!</p>
-            </li>
-            <li>
-              <p className="w3-slim w3-text-dark-grey"><b>dorian: </b>{DummyText}</p>
-            </li>
+            {this.generateMessages()}
           </ul>
         </main>
         <footer className="panel__footer">
-          <textarea className="w3-input w3-border" placeholder="Enter message..." style={{resize:'none'}}></textarea>
+          <textarea className="w3-input w3-border" style={{resize:'none'}}
+            onChange={event => this.handleInput(event)}
+            onKeyPress={event => this.handleSubmit(event)}
+            placeholder="Enter message..."
+            value={this.state.message}
+          />
         </footer>
       </div>
     );
@@ -49,10 +105,15 @@ class Chat extends Component {
  
 Chat.propTypes = {
   features_show: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  chats: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
  
 export default createContainer(() => {
+  Meteor.subscribe('chats');
   return {
     features_show: AppState.get('features_show'),
+    username: AppState.get('user_name'),
+    chats: Chats.find({}, { sort: { createdAt: 1 } }).fetch(),
   };
 }, Chat);
